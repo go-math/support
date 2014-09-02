85c1eadd49f764549662a40fd0785cd1df8a1600
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-02 17:23:30 +0200

    Added some documentation

--- assert/main.go
+++ assert/main.go
@@ -1,3 +1,5 @@
+// Package assert provides a number of functions for making assertions in
+// tests.
 package assert
 
 import (
@@ -5,24 +7,29 @@ import (
 	"testing"
 )
 
+// Equal asserts that two objects are equal
 func Equal(actual, expected interface{}, t *testing.T) {
 	if actual != expected {
 		t.Fatalf("got %v (%T) instead of %v (%T)", actual, actual, expected, expected)
 	}
 }
 
+// DeepEqual asserts that two objects with nontrivial types, such as structs
+// and slices, are equal.
 func DeepEqual(actual, expected interface{}, t *testing.T) {
 	if reflect.DeepEqual(actual, expected) {
-		t.Fatalf("got %v instead of %v", actual, expected)
+		t.Fatalf("got %v (%T) instead of %v (%T)", actual, actual, expected, expected)
 	}
 }
 
+// Success asserts that the error is nil.
 func Success(err error, t *testing.T) {
 	if err != nil {
 		t.Fatalf("got an error '%v'", err)
 	}
 }
 
+// Success asserts that the error is not nil.
 func Failure(err error, t *testing.T) {
 	if err == nil {
 		t.Fatalf("expected an error")
