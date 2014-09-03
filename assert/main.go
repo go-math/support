8da5223ae69fb201f8dfbbe4ef1831206fb5e692
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-03 15:12:48 +0200

    Report the file and line when an assertion fails

--- assert/main.go
+++ assert/main.go
@@ -4,13 +4,14 @@ package assert
 
 import (
 	"reflect"
+	"runtime"
 	"testing"
 )
 
 // Equal asserts that two objects are equal
 func Equal(actual, expected interface{}, t *testing.T) {
 	if actual != expected {
-		t.Fatalf("got %v (%T) instead of %v (%T)", actual, actual, expected, expected)
+		raise(t, "got %v (%T) instead of %v (%T)", actual, actual, expected, expected)
 	}
 }
 
@@ -18,20 +19,27 @@ func Equal(actual, expected interface{}, t *testing.T) {
 // and slices, are equal.
 func DeepEqual(actual, expected interface{}, t *testing.T) {
 	if !reflect.DeepEqual(actual, expected) {
-		t.Fatalf("got %v (%T) instead of %v (%T)", actual, actual, expected, expected)
+		raise(t, "got %v (%T) instead of %v (%T)", actual, actual, expected, expected)
 	}
 }
 
 // Success asserts that the error is nil.
 func Success(err error, t *testing.T) {
 	if err != nil {
-		t.Fatalf("got an error '%v'", err)
+		raise(t, "got an error '%v'", err)
 	}
 }
 
 // Success asserts that the error is not nil.
 func Failure(err error, t *testing.T) {
 	if err == nil {
-		t.Fatalf("expected an error")
+		raise(t, "expected an error")
 	}
+}
+
+func raise(t *testing.T, format string, args ...interface{}) {
+	if _, file, line, ok := runtime.Caller(2); ok {
+		t.Errorf("%s:%d", file, line)
+	}
+	t.Fatalf(format, args...)
 }
