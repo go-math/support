74891d87fea5ff0590e57e2aaf9c8f4cd823490f
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-08 16:35:11 +0200

    Added MakeMatrix in fixture

--- fixture/main.go
+++ fixture/main.go
@@ -6,16 +6,27 @@ import (
 	"math/rand"
 )
 
-// MakeSymMatrix returns a random symmetric matrix.
-func MakeSymMatrix(n uint) []float64 {
-	M := make([]float64, n*n)
+// MakeMatrix generates a random matrix.
+func MakeMatrix(m, n uint32) []float64 {
+	M := make([]float64, m*n)
 
-	for i := uint(0); i < n; i++ {
-		M[i*n+i] = rand.Float64()
+	for i := range M {
+		M[i] = rand.Float64()
+	}
 
-		for j := i + 1; j < n; j++ {
-			M[i*n+j] = rand.Float64()
-			M[j*n+i] = M[i*n+j]
+	return M
+}
+
+// MakeSymMatrix generates a random, symmetric matrix.
+func MakeSymMatrix(m uint32) []float64 {
+	M := make([]float64, m*m)
+
+	for i := uint32(0); i < m; i++ {
+		M[i*m+i] = rand.Float64()
+
+		for j := i + 1; j < m; j++ {
+			M[i*m+j] = rand.Float64()
+			M[j*m+i] = M[i*m+j]
 		}
 	}
 
