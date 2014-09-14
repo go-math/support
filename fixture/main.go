f2ddc3ebcacabf48da7f3af09a79c3acababb2c0
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-14 14:08:08 +0200

    Fixed matrix generators into a separate file

--- fixture/main.go
+++ fixture/main.go
@@ -1,34 +1,3 @@
 // Package fixture provides a number of functions for managing fixture data in
 // tests.
 package fixture
-
-import (
-	"math/rand"
-)
-
-// MakeMatrix generates a random matrix.
-func MakeMatrix(m, n uint32) []float64 {
-	M := make([]float64, m*n)
-
-	for i := range M {
-		M[i] = rand.Float64()
-	}
-
-	return M
-}
-
-// MakeSymMatrix generates a random, symmetric matrix.
-func MakeSymMatrix(m uint32) []float64 {
-	M := make([]float64, m*m)
-
-	for i := uint32(0); i < m; i++ {
-		M[i*m+i] = rand.Float64()
-
-		for j := i + 1; j < m; j++ {
-			M[i*m+j] = rand.Float64()
-			M[j*m+i] = M[i*m+j]
-		}
-	}
-
-	return M
-}
